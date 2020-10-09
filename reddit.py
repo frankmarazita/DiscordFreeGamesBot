039d87ac70310d9d0be39518f4cae9c9762aec6e
Frank Marazita <fmarazita1@gmail.com>
2020-10-10 00:25:39 +1100

    now keeps posts in memory not file

--- reddit.py
+++ reddit.py
@@ -16,22 +16,17 @@ DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 DISCORD_CHANNEL = int(os.getenv('DISCORD_CHANNEL'))
 client = discord.Client()
 
-if not os.path.exists('./posts.txt'):
-    open("posts.txt", "w+")
+posts = []
 
 def getFreeGames():
 
     messages = []
 
-    f = open("posts.txt", "r")
-    already_done = f.readlines()
-
     for submission in subreddit.new(limit=10):
         op_text = submission.selftext.lower()
-        if submission.name + '\n' not in already_done:
+        if submission.name not in posts:
             if any(keyword in submission.url for keyword in keywords):
-                f = open("posts.txt", "a")
-                f.write(submission.name + '\n')
+                posts.append(submission.name)
                 title = bs4.BeautifulSoup(requests.get(
                     submission.url).text, features="html.parser").title.text
                 messages.append(title + "\n" + submission.url)
